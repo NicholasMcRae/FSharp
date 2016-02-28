61cb2f17fa3d539cedd98e7f6d4d33063ed49310
NicholasMcRae <nicholas.mcrae86@gmail.com>
2016-02-28 13:49:39 -0500

    Started new project

---
+++ FSharp_28022016/FSharp_28022016/FSharp_28022016/Program.fs
@@ -0,0 +1,17 @@
+﻿// function that returns a function to
+let calculatePrefixFunction prefix =
+    // calculate prefix
+    let prefix' = Printf.sprintf "[%s]: " prefix
+    // define function to perform prefixing
+    let prefixFunction appendee =
+        Printf.sprintf "%s%s" prefix' appendee
+    // return function
+    prefixFunction
+    
+// create the prefix function
+let prefixer = calculatePrefixFunction "DEBUG"
+
+// use the prefix function
+printfn "%s" (prefixer "My message")
+
+System.Console.ReadLine()
