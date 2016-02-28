c41bf1f70d519fc5515dd57ba812bde0c1ad99fc
NicholasMcRae <nicholas.mcrae86@gmail.com>
2016-02-27 20:29:41 -0500

    First F Sharp program

---
+++ FSharp_27022016/FSharp_27022016/Program.fs
@@ -0,0 +1,18 @@
+﻿(*
+    Feb. 27 / 2016
+    First F# program
+*)
+
+open System
+
+[<EntryPoint>]
+let main argv = 
+    
+    let sign num =
+       if num > 0 then "positive"
+       elif num < 0 then "negative"
+       else "zero"
+
+    Console.WriteLine(sign 10)
+
+    0 // return an integer exit code
