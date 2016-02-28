d4e25f8fc62a52e13f8c86ca6784a9c6f155de18
NicholasMcRae <nicholas.mcrae86@gmail.com>
2016-02-28 14:03:49 -0500

    Following example in succinctly F#

---
+++ FSharp_28022016/FSharp_28022016/FSharp_28022016/Program.fsx
@@ -0,0 +1,10 @@
+﻿#load "FSharpChart.fsx"
+
+open System 
+open MSDN.FSharp.Charting
+
+let dateInApril day = new DateTime(2012, 03, day)
+
+let data = [ dateInApril 6, 4; dateInApril 7, 8; 
+             dateInApril 8, 2; dateInApril 9, 3 ]
+FSharpChart.Column data
