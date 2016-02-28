856d0f0ffc847b5f5667f22dbc0ae3cd88a244f9
NicholasMcRae <nicholas.mcrae86@gmail.com>
2016-02-28 14:31:10 -0500

    New project

--- FSharp_28022016_2/FSharp_28022016_2/AssemblyInfo.fs
+++ FSharp_28022016_2/FSharp_28022016_2/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace FSharp_28022016.AssemblyInfo
+﻿namespace FSharp_28022016_2.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("FSharp_28022016")>]
+[<assembly: AssemblyTitle("FSharp_28022016_2")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("FSharp_28022016")>]
+[<assembly: AssemblyProduct("FSharp_28022016_2")>]
 [<assembly: AssemblyCopyright("Copyright ©  2016")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
@@ -22,7 +22,7 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("b367daf8-269c-49f8-9beb-44672d2421ca")>]
+[<assembly: Guid("70aca86e-d1a1-4dfc-9460-0c35c7f6a77e")>]
 
 // Version information for an assembly consists of the following four values:
 // 
---
+++ FSharp_28022016_2/FSharp_28022016_2/Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+let testString = "Some text"
+
+let testFunction = fun x y -> x + y
+
+let raisePowerTwo x = x ** 2.0
